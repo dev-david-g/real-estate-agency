e85b390a00450714522709f37812e71b9ec844d3
David Gergely <dgergely03@gmail.com>
2024-03-19 12:45:46 +0200

    incldue angular material 17.3 to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,10 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import { AuthInterceptorService } from './core/auth-interceptor.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(),
   provideHttpClient(withFetch()),
-  {provide: HTTP_INTERCEPTORS, useValue: AuthInterceptorService}]
+  {provide: HTTP_INTERCEPTORS, useValue: AuthInterceptorService}, provideAnimationsAsync()]
 };
